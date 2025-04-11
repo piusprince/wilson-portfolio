082d719bc6fe29f86e12eee378fba8c558dd98fe
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-04-11 18:35:45 +0000

    Add image remote patterns configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "a.storyblok.com",
+        port: "",
+        pathname: "/f/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
