f5dec24932845bd5d6db72ffcf280e64b8bb2413
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-04-11 18:36:30 +0000

    Add fetchStory function to interact with Storyblok API

---
+++ app/_actions/fetchStory.ts
@@ -0,0 +1,27 @@
+import { ISbStoriesParams } from "@storyblok/react";
+import { ISbResponseData } from "storyblok-js-client";
+
+import { getStoryblokApi } from "@/lib/storyblok";
+
+export async function fetchStory({
+  slug,
+  params = {},
+}: {
+  slug: string;
+  params?: ISbStoriesParams;
+}): Promise<ISbResponseData> {
+  const api = getStoryblokApi();
+
+  if (!api) {
+    throw new Error("Storyblok API client is not available");
+  }
+
+  const fullSlug = slug.startsWith("cdn/stories")
+    ? slug
+    : `cdn/stories/${slug}`;
+
+  const response = await api.get(fullSlug, params, {
+    cache: "no-store",
+  });
+  return response.data;
+}
