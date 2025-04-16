bc178ba2d81173224653ae667a8885962d698b5d
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-04-16 23:24:35 +0000

    feat: Implement Text component for rendering rich text with Storyblok integration

---
+++ components/text.tsx
@@ -0,0 +1,31 @@
+import { options } from "@/lib/richtextUtils";
+import {
+  convertAttributesInElement,
+  richTextResolver,
+  storyblokEditable,
+} from "@storyblok/react";
+
+type TextProps = {
+  _uid: string;
+  text: any;
+  component: string;
+  _editable?: string;
+};
+
+export const Text = ({ blok }: { blok: TextProps }) => {
+  const html = richTextResolver(options).render(blok.text);
+  const formattedHtml = convertAttributesInElement(html);
+
+  return (
+    <div
+      {...storyblokEditable(blok)}
+      data-testid="text"
+      id={blok._uid}
+      className="w-full relative overflow-hidden"
+    >
+      {formattedHtml && (
+        <div className="prose prose-lg max-w-none ">{formattedHtml}</div>
+      )}
+    </div>
+  );
+};
