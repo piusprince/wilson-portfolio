f2de0ab301bfec8c99edc838a2f0e5091ce72244
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-04-17 16:54:59 +0000

    refactor: Improve getStoryblokLinkUrl function and enhance StoryblokLink type definition

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,31 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export type StoryblokLink = {
+  id: string;
+  url: string;
+  target?: string;
+  linktype: "story" | "url" | "email" | string;
+  fieldtype: "multilink";
+  cached_url?: string;
+  email?: string;
+};
+
+export function getStoryblokLinkUrl(link: StoryblokLink): string {
+  if (!link) return "#";
+
+  switch (link.linktype) {
+    case "story":
+      return `/${link.cached_url}`;
+    case "url":
+      return link.url;
+    case "email":
+      return `mailto:${link.email}`;
+    default:
+      return "#";
+  }
 }
