befc90a391aef809007a53941a3a732638acd356
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-04-22 09:52:48 +0000

    feat: Refactor CaseStudyIntro component to use section element for improved semantics and layout

--- components/case-study-intro.tsx
+++ components/case-study-intro.tsx
@@ -34,38 +34,51 @@ export const CaseStudyIntro = ({ blok }: CaseStudyIntroBlok) => {
   };
 
   return (
-    <div {...storyblokEditable(blok)} className="max-w-3xl mb-[52px]">
-      {headlineBlok && (
-        <Headline
-          className="text-black"
-          level={headlineBlok.level}
-          content={headlineBlok.content}
-        />
-      )}
-      <Text blok={textComponentBlok} />
+    <section
+      {...storyblokEditable(blok)}
+      className="max-w-[1280px] mx-auto mb-[52px]"
+    >
+      <div className="max-w-3xl">
+        {headlineBlok && (
+          <Headline
+            className="text-black"
+            level={headlineBlok.level}
+            content={headlineBlok.content}
+          />
+        )}
+        <Text blok={textComponentBlok} />
 
-      <div className="grid grid-cols-4 gap-6 text-sm pt-4 mt-8">
-        <div>
-          <p className="font-medium text-gray-500 mb-1">Year</p>
-          <p>{blok.case_study_year}</p>
-        </div>
-        <div>
-          <p className="font-medium text-gray-500 mb-1">Industry</p>
-          <p>{blok.case_study_industry}</p>
-        </div>
-        <div>
-          <p className="font-medium text-gray-500 mb-1">Role</p>
-          <p>{blok.case_study_role}</p>
-        </div>
-        <div>
-          <p className="font-medium text-gray-500 mb-1">Tools</p>
-          <p>
-            {Array.isArray(blok.case_study_tools)
-              ? blok.case_study_tools.join(", ")
-              : blok.case_study_tools}
-          </p>
+        <div className="grid grid-cols-4 gap-6 pt-4 mt-8 text-sm">
+          {blok.case_study_year && (
+            <div>
+              <p className="mb-1 font-medium text-gray-500">Year</p>
+              <p>{blok.case_study_year}</p>
+            </div>
+          )}
+          {blok.case_study_industry && (
+            <div>
+              <p className="mb-1 font-medium text-gray-500">Industry</p>
+              <p>{blok.case_study_industry}</p>
+            </div>
+          )}
+          {blok.case_study_role && (
+            <div>
+              <p className="mb-1 font-medium text-gray-500">Role</p>
+              <p>{blok.case_study_role}</p>
+            </div>
+          )}
+          {blok.case_study_tools && blok.case_study_tools.length > 0 && (
+            <div>
+              <p className="mb-1 font-medium text-gray-500">Tools</p>
+              <p>
+                {Array.isArray(blok.case_study_tools)
+                  ? blok.case_study_tools.join(", ")
+                  : blok.case_study_tools}
+              </p>
+            </div>
+          )}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
