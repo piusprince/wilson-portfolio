fd998968aca34f3a8b20728f11349a97396d986a
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-04-25 17:28:39 +0000

    fix: Refactor TextImageGrid component to use next/image and streamline image rendering

--- components/text-image-grid.tsx
+++ components/text-image-grid.tsx
@@ -3,7 +3,8 @@
 import { storyblokEditable } from "@storyblok/react";
 
 import { Text } from "@/components/text";
-import { ImageAtom, type ImageProps } from "@/components/image";
+import { type ImageProps } from "@/components/image";
+import Image from "next/image";
 
 interface TextComponentProps {
   _uid: string;
@@ -29,28 +30,55 @@ export default function TextImageGrid({ blok }: TextImageGridProps) {
       {...storyblokEditable(blok)}
       data-testid="text-image-grid"
       id={blok._uid}
-      className="container mx-auto py-16 pl-4 pr-6 md:px-[50px] lg:px-0 max-w-[1283px]"
+      className="container mx-auto pl-4 pr-6 md:px-[50px] lg:px-0 max-w-[1283px]"
     >
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12">
-        <div className="space-y-6">
-          {blok.text?.map((textBlok) => (
-            <Text key={textBlok._uid} blok={textBlok} />
-          ))}
-        </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-[38px]">
+        {blok.text?.map((textBlok) => (
+          <Text
+            key={textBlok._uid}
+            hasMarginBottom={false}
+            blok={textBlok}
+            className="!px-0"
+          />
+        ))}
 
         <div className="grid grid-cols-2 gap-4">
           {blok.image_grid?.map((imageBlok, index) => {
             if (index === 0) {
               return (
-                <div key={imageBlok._uid} className="col-span-2">
-                  <ImageAtom blok={imageBlok} />
+                <div
+                  key={imageBlok._uid}
+                  className="col-span-2 relative w-full h-[370px] rounded-2xl overflow-hidden"
+                >
+                  <Image
+                    data-testid="image"
+                    id={imageBlok._uid}
+                    src={imageBlok.image.filename}
+                    alt={imageBlok.image.alt}
+                    // className="w-full h-[650px] object-cover rounded-2xl"
+                    sizes="(max-width: 768px) 100vw, 50vw"
+                    priority
+                    fill
+                  />
                 </div>
               );
             }
 
             return (
-              <div key={imageBlok._uid}>
-                <ImageAtom blok={imageBlok} />
+              <div
+                key={imageBlok._uid}
+                className="relative w-full h-[370px] rounded-2xl overflow-hidden"
+              >
+                <Image
+                  data-testid="image"
+                  id={imageBlok._uid}
+                  src={imageBlok.image.filename}
+                  alt={imageBlok.image.alt}
+                  className="w-full h-full object-cover rounded-2xl"
+                  sizes="(max-width: 768px) 100vw, 50vw"
+                  priority={index === 1}
+                  fill
+                />
               </div>
             );
           })}
